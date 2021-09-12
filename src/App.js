c9f0200a6d16da62aa01188b7b7e0883e3489b9e
Vickydew1 <vickydewangan581@gmail.com>
2021-09-12 12:46:07 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>h111</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is homepage
+      </Route>
+
+      <Route exact path="/Starred">
+        This is Starred
+      </Route>
+
+      <Route>NOT found page</Route>
+    </Switch>
+  );
 }
 
 export default App;
